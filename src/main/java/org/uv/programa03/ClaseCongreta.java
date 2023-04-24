77be5748b335524888b96818b25a9e703d28802d
FhatimaReyes <fhareal10@gmail.com>
2023-04-24 08:55:22 -0600

    inicializacion

---
+++ src/main/java/org/uv/programa03/ClaseCongreta.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.uv.programa03;
+
+/**
+ *
+ * @author FHATIMA
+ */
+public class ClaseCongreta {
+    public void imprimir(){
+    System.out.println("hola mundo");
+    }
+}
